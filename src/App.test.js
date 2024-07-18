e06a3168fd60a5890627da11fa721f62eed03d93
Nasim Yousefi <nasim.yousefi05@gmail.com>
2024-07-18 13:57:51 +0330

    fix error 3

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,17 @@
-import	{	render,	screen	}	from	'@testing-library/react';
-import	App	from	'./App';
-test('renders	Hello,	world!	text',	()	=>	{
-		render(<App	/>);
-		const	linkElement	=	screen.getByText(/Hello,	world!/i);
-		expect(linkElement).toBeInTheDocument();
+// import	{	render,	screen	}	from	'@testing-library/react';
+// import	App	from	'./App';
+// test('renders	Hello,	world!	text',	()	=>	{
+// 		render(<App	/>);
+// 		const	linkElement	=	screen.getByText(/Hello,	world!/i);
+// 		expect(linkElement).toBeInTheDocument();
+// });
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Hello, world! text', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello, world!/i);
+  expect(linkElement).toBeInTheDocument();
 });
